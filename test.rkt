9ded6c945865f38616b9e2de8c029e1ae2897583
Wenbo Sun <bracksun@hotmail.com>
2016-12-08 23:16:26 +0800

    new file:   test.rkt

---
+++ test.rkt
@@ -0,0 +1,88 @@
+#lang racket
+
+#| The underlying principles of the autograding framework is simple. 
+We use the rackunit unit testing framework that comes with the racket
+distrobution. We have define a set of calls that take a test-suite.
+and executes the test-suite with eval rebound to a sandboxed evaluator
+that provides racket and the file being tested. Disk access should be
+limited to those files and time and space are limited by the parameters
+time-for-eval and space-for-eval. 
+|#
+
+(require rackunit rackunit/text-ui racket/sandbox)
+(provide test-file)
+
+#|
+ Test File is the minimum requirement for being able to understand
+ our the autograder. test-file is a function that when invoked with
+ no arguments will search will the current directory for the file 
+ named a1.rkt and run the test suite with that file.
+ If a single argument is provided that argument must be the relative or
+ absolute path to the file containing the definitions for the assignment.
+|#
+
+(define test-file
+  (lambda (#:file-name (file "./ch2.rkt")
+	   #:sec-of-eval (sec 5)
+	   #:mb-of-eval (mb 5))
+    (parameterize ((read-accept-reader #t)
+                   (read-accept-lang #t))
+      (let ((sandboxed-eval
+             (make-module-evaluator (read (open-input-file file))
+                                    ;; #:allow-for-require '(C311/pmatch)
+                                    )))
+        (set-eval-limits sandboxed-eval sec mb)
+        (parameterize ((current-eval sandboxed-eval)
+	               (error-print-context-length 0))
+          (run-tests tests))))))
+
+#|
+  A tests is the name of the test-suite that is run by a call to test-file.
+The test suite is a type that is define in the rackunit module. I will give
+some examples of how test might be structured. If furthure documentation is
+require feel free to browse the rackunit documentation at the following address.
+http://docs.racket-lang.org/rackunit/?q=rackunit
+|#
+
+    
+(define tests
+  (test-suite "ch2"
+    (test-suite "uniquify"
+      (test-equal-if-defined uniquify	      
+        ;; Making sure set! works
+        (((uniquify '())
+          '(program
+             (let ([x 32])
+                (+ (let ([x 10]) x) x))))
+
+
+(define-syntax test-if-defined
+  (syntax-rules ()
+    ((_ sym tests ...)
+     (test-case (format "~a undefined" 'sym)
+                (check-not-false (lambda () (eval 'sym)))
+                tests ...))))
+
+(define-syntax test-equal-if-defined
+  (syntax-rules ()
+    ((_ ident (expr val) ...)
+      (let ((n 1))
+        (test-case (format "~a: undefined" 'ident)
+                   (check-not-exn (lambda () (eval 'ident)))
+                   (test-case (format "~a: ~a" 'ident n)
+                              (with-check-info 
+                               (('tested 'expr))
+                               (set! n (add1 n))
+                               (check equal? (eval 'expr) val))) ...)))))
+
+(define-syntax ifdef-suite
+  (syntax-rules ()
+    ((_ ident (expr val) ...)
+     (let ((n 1))
+       (test-suite (~a 'ident)
+        (test-case "undefined"
+         (check-not-exn (lambda () (eval 'ident)))
+         (test-case (~a n)
+          (with-check-info (('tested 'expr))
+           (set! n (add1 n))
+           (check equal? (eval 'expr) val))) ...))))))
